97992a90fa3e4df809bba028ca156ddc3b492286
Matheus Viana <matheus_o_viana@hotmail.com>
2023-01-21 22:55:46 -0300

    Add datepicker, add some modifiers to it and select a range to it

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,10 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 0.0
+    @State private var wakeUp = Date.now
     
     var body: some View {
-        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+        DatePicker("Please enter a time", selection: $wakeUp, in: Date.now...)
+            .labelsHidden()
+        
     }
 }
 
