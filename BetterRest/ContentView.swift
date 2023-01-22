4eca93d3c6c7838cbba1c3552757b3f4c6b480a3
Matheus Viana <matheus_o_viana@hotmail.com>
2023-01-21 23:24:05 -0300

    add coreML training model

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -9,14 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text(Date.now.formatted(date: .long, time: .complete))
-    }
-    
-    func trivialExample() {
-        let compoenents = Calendar.current.dateComponents([.hour, .minute], from: Date.now)
-        let hour = compoenents.hour ?? 0
-        let minute = compoenents.minute ?? 0
-        
+        Text("Hello, world!")
+            .padding()
     }
 }
 
